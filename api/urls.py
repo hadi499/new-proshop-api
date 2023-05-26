80ca5f9452937cf5a90b4cb2041ed9635a3308c6
hadi499 <hadi.ygcl@gmail.com>
2023-05-26 12:56:57 +0700

    update product

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts),
     path('products/create/', views.createProduct, name="product-create"),
+    path('products/upload/', views.uploadImage, name="image-upload"),
     path('products/<str:pk>/', views.getProduct),
     path('products/update/<str:pk>/', views.updateProduct, name="product-update"),
     path('products/delete/<str:pk>/', views.deleteProduct, name="product-delete"),
